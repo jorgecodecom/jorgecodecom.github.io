5b0d2ec1bd5836565f7663be18b299464b4d5d6a
jorge <jorge@jorgecode.com>
2019-10-14 20:10:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# jorgecode.com menu 
+
+A small user friendly menu of some jorgecode projects in github page.
+[Go to the application](https://jorgecodecom.github.io)
+
+
+License
+----
+MIT
+
+## About Me
+[Me](http://jorgecode.com/jorge).
+
+[My Blog](http://jorgecode.com).
---
+++ pokedex/precache-manifest.b9517e7dad29a86eb910affe0bd2e595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fedbad570ab6dcc17968203529b4009",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba905ff7a639c5ee22a8",
+    "url": "./static/css/main.69bb9b2b.chunk.css"
+  },
+  {
+    "revision": "3b2e15c1a19f762ac4b4",
+    "url": "./static/js/2.16d596e4.chunk.js"
+  },
+  {
+    "revision": "ba905ff7a639c5ee22a8",
+    "url": "./static/js/main.67f2e89c.chunk.js"
+  },
+  {
+    "revision": "f2edb5ba7e19e24da3f7",
+    "url": "./static/js/runtime-main.e6cdb6f2.js"
+  }
+]);
